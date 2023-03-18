8c479a4457803ec2e25846d12f4e07512163a95f
bartieres <andre.bartieres@gmail.com>
2023-03-18 19:57:42 -0300

    Implementa autorizacao de usuario

--- src/main/java/bartieres/crud/springboot3/infra/security/TokenService.java
+++ src/main/java/bartieres/crud/springboot3/infra/security/TokenService.java
@@ -4,6 +4,8 @@ import bartieres.crud.springboot3.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +29,20 @@ public class TokenService {
                     .sign(algoritimo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        DecodedJWT decodedJWT;
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    .withIssuer("API CRUD Bartieres")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
