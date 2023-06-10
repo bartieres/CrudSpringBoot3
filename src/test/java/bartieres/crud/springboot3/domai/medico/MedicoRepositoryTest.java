37d50f74886c44bc2fd72ed51b8b7bd1f4d2f551
bartieres <andre.bartieres@gmail.com>
2023-06-10 17:54:00 -0300

    Remove teste com banco mysql e adiciona H2

--- src/test/java/bartieres/crud/springboot3/domai/medico/MedicoRepositoryTest.java
+++ src/test/java/bartieres/crud/springboot3/domai/medico/MedicoRepositoryTest.java
@@ -1,13 +1,9 @@
 package bartieres.crud.springboot3.domai.medico;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
 public class MedicoRepositoryTest {
 
     @Test
